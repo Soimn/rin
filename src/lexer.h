82bc085b8f3f1580804cd6cb1982ff204fc1df0e
Simon Doksrød <soimn.dok@gmail.com>
2023-11-10 14:36:11 +0100

    ux lab bu

--- src/lexer.h
+++ src/lexer.h
@@ -1,6 +1,65 @@
+#define TOKEN_BLOCK(IDX) ((IDX) << 4)
+
 typedef enum Token_Kind
 {
 	Token_Invalid = 0,
 
-	Token_
+  Token_String,
+  Token_Char,   // TODO
+	Token_Ident,
+
+  Token_OpenParen,
+  Token_CloseParen,
+  Token_OpenBracket,
+  Token_CloseBracket,
+  Token_OpenBrace,
+  Token_CloseBrace,
+  Token_Period,
+  Token_Colon,
+  Token_Comma,
+  Token_Semicolon,
+  Token_Hat,
+  Token_At,
+  Token_Arrow,
+  Token_PlusPlus,   // TODO
+  Token_MinusMinus, // TODO
+  Token_TripleMinus,
+  Token_Tilde,
+  
+  Token__FirstMulLevel = TOKEN_BLOCK(1),
+  Token_Star = Token__FirstMulLevel,
+  Token_Slash,
+  Token_Mod,
+  Token_And,
+  Token_LeLe,
+  Token_GeGe,
+  Token_GeGeGe,
+  Token__PastLastMulLevel,
+
+  Token__FirstAddLevel = TOKEN_BLOCK(2),
+  Token_Plus = Token__FirstAddLevel,
+  Token_Minus,
+  Token_Pipe,
+  Token__PastLastAddLevel,
+
+  Token__FirstCmpLevel = TOKEN_BLOCK(3),
+  Token_EqualsEQ,
+  Token_Le,
+  Token_LeEQ,
+  Token_Ge,
+  Token_GeEQ,
+  Token__PastLastCmpLevel,
+
+  Token__FirstAndLevel = TOKEN_BLOCK(4),
+  Token_AndAnd = Token__FirstAndLevel,
+  Token__PastLastAndLevel,
+
+  Token__FirstOrLevel = TOKEN_BLOCK(5),
+  Token_OrOr = Token__FirstOrLevel,
+  Token__PastLastOrLevel,
+
+  Token_Qmark,
 } Token_Kind;
+
+STATIC_ASSERT(Token__PastLastMulLevel <= Token__FirstAddLevel);
+STATIC_ASSERT(Token__PastLastAddLevel <= Token__FirstCmpLevel);
--- src/rin.h
+++ src/rin.h
@@ -3,10 +3,25 @@ typedef signed __int16 i16;
 typedef signed __int32 i32;
 typedef signed __int64 i64;
 
+#define I8_MIN   (i8)(0x80)
+#define I16_MIN (i16)(0x8000)
+#define I32_MIN (i32)(0x80000000)
+#define I64_MIN (i64)(0x8000000000000000)
+
+#define I8_MAX   (i8)(0x7F)
+#define I16_MAX (i16)(0x7FFF)
+#define I32_MAX (i32)(0x7FFFFFFF)
+#define I64_MAX (i64)(0x7FFFFFFFFFFFFFFF)
+
 typedef unsigned __int8  u8;
 typedef unsigned __int16 u16;
 typedef unsigned __int32 u32;
 typedef unsigned __int64 u64;
+
+#define U8_MAX   (u8)(0xFF)
+#define U16_MAX (u16)(0xFFFF)
+#define U32_MAX (u32)(0xFFFFFFFF)
+#define U64_MAX (u64)(0xFFFFFFFFFFFFFFFF)
 
 typedef i64 imm;
 typedef u64 umm;
